cfa6481c54fff1c825e9ca4ab85f43911bead51f
Sean Zhou <seanzhousiyuan@outlook.com>
2021-10-02 20:30:31 +0800

    Page template and app settings

---
+++ src/shared/AppSettings.tsx
@@ -0,0 +1,75 @@
+import React, { useContext } from 'react';
+import { useTranslation } from 'react-i18next';
+import { RadioChangeEvent, Radio, Switch } from 'antd';
+import { I18nKeys } from 'result/i18n-keys';
+import { ReactComponent as CloseIcon } from 'assets/icon-close.svg';
+import { allLanguages, Language, StorageKey } from './models';
+import { SaveSubmissionFlagContext } from './saveSubmissionFlagContext';
+
+interface AppSettingsProps {
+  onClose: () => void;
+}
+
+const languageLabels: Record<Language, string> = {
+  [Language.En]: 'English',
+  [Language.Zh]: '繁體中文',
+  [Language.ZhYue]: '粵語（香港）',
+};
+
+function AppSettings({ onClose }: AppSettingsProps) {
+  const { t, i18n } = useTranslation();
+  const { saveSubmissionFlag, setSaveSubmissionFlag } = useContext(SaveSubmissionFlagContext);
+  const handleChangeLanguage = (event: RadioChangeEvent) => {
+    const lang = event.target.value;
+    i18n.changeLanguage(lang);
+    localStorage.setItem(StorageKey.Lang, lang);
+  };
+
+  return (
+    <div className="app-settings">
+      <div className="width-container">
+        <header className="app-settings__header">
+          <span>{t(I18nKeys.Settings)}</span>
+          <button className="app-settings__close-button" onClick={() => onClose()}>
+            <CloseIcon />
+          </button>
+        </header>
+        <div className="setting">
+          <div className="setting__name">語言/Language</div>
+          <div className="setting__content">
+            <Radio.Group
+              value={i18n.resolvedLanguage}
+              onChange={handleChangeLanguage}
+              className="select-language-control"
+            >
+              {allLanguages.map((lang) => (
+                <Radio key={lang} value={lang} className="select-language-control__radio">
+                  {languageLabels[lang]}
+                </Radio>
+              ))}
+            </Radio.Group>
+          </div>
+        </div>
+        <div className="setting">
+          <div className="setting__name">{t(I18nKeys.SaveSubmissionResults)}</div>
+          <div className="setting__content">
+            <Switch
+              checked={saveSubmissionFlag}
+              onChange={setSaveSubmissionFlag}
+              className="toggle-keep-submission"
+            />
+            <span>
+              {t(
+                saveSubmissionFlag
+                  ? I18nKeys.SaveResultsParagraph
+                  : I18nKeys.DoNotSaveResultsParagraph
+              )}
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AppSettings;
--- src/shared/PageTemplate.tsx
+++ src/shared/PageTemplate.tsx
@@ -1,17 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useLocation } from 'react-router-dom';
+import classNames from 'classnames';
+import { I18nKeys } from 'result/i18n-keys';
+import AppSettings from './AppSettings';
 
-interface Props {
-  children: React.ReactNode;
-}
+function PageTemplate({ children }: { children: React.ReactNode }) {
+  const location = useLocation();
+  const { t } = useTranslation();
+  const [showSettings, setShowSettings] = useState(false);
+  const toggleSettings = () => setShowSettings(!showSettings);
 
-function PageTemplate({ children }: Props) {
+  useEffect(() => {
+    if (showSettings) {
+      setShowSettings(false);
+    }
+  }, [location]);
+
   return (
     <div className="page">
-      {children}
-
-      <footer>
-        <span>© Sean Zhou</span>
-      </footer>
+      <header className="page-header">
+        <div className="width-container">
+          <div className="page-header__inner">
+            <div className="app-logo">{t(I18nKeys.CalorieCalculator)}</div>
+            <button
+              className={classNames('toggle-settings-button', showSettings ? 'open' : null)}
+              onClick={toggleSettings}
+            >
+              {t(I18nKeys.Settings)}
+            </button>
+          </div>
+        </div>
+        {!showSettings ? null : <AppSettings onClose={() => setShowSettings(false)} />}
+      </header>
+      <main className="page-body">{children}</main>
     </div>
   );
 }
