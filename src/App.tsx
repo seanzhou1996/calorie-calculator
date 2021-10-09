e980bd64f4144dfa273e4f7d7813a6eb5ae3b9af
Sean Zhou <seanzhousiyuan@outlook.com>
2021-10-09 15:32:46 +0800

    Always scroll to top on route change

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import 'shared/i18n';
 import { AllFormDataContextProvider } from 'shared/allFormDataContext';
+import { getSubmission, getStoreSubmissionFlag } from 'shared/store';
 import PageTemplate from 'shared/PageTemplate';
+import ScrollToTop from 'shared/ScrollToTop';
 import Home from './home/Home';
 import Activity from './activity/Activity';
 import Goal from './goal/Goal';
-import { getSubmission, getStoreSubmissionFlag } from 'shared/store';
 import Result from './result/Result';
+import HowItWorks from 'HowItWorks';
 import LastSubmissionBanner from 'shared/LastSubmissionBanner';
 import { SaveSubmissionFlagContextProvider } from 'shared/saveSubmissionFlagContext';
 import { ConfigProvider } from 'antd';
@@ -41,8 +43,13 @@ function App() {
                 <Route path="/result">
                   <Result />
                 </Route>
+
+                <Route path="/how-it-works">
+                  <HowItWorks />
+                </Route>
               </Switch>
             </PageTemplate>
+            <ScrollToTop />
           </Router>
         </AllFormDataContextProvider>
       </SaveSubmissionFlagContextProvider>
