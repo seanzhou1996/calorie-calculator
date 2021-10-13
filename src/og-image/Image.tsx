0079ff4a31f30502baf0111b049e08c0e3681d72
Sean Zhou <seanzhousiyuan@outlook.com>
2021-10-13 10:52:32 +0800

    og image temp

---
+++ src/og-image/Image.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function Image() {
+  return (
+    <div className="og-image-wrapper">
+      <div className="og-image-container">
+        <div className="og-title">
+          <div className="og-title__zh">
+            <span>卡路里計數機</span>
+          </div>
+          <div className="og-title__en">
+            <span>Calorie Calculator</span>
+          </div>
+          <div className="og-title__author">
+            <span>By Sean Zhou</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Image;
